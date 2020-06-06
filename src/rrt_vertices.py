cee6fec8737453563dbd783a7710bf1bd74f506f
JaroslawBozek <48987078+JaroslawBozek@users.noreply.github.com>
2020-06-06 21:06:56 +0200

    Update rrt_vertices.py

--- src/rrt_vertices.py
+++ src/rrt_vertices.py
@@ -13,39 +13,11 @@ class RRT(GridMap):
     def __init__(self):
         super(RRT, self).__init__()
         self.max_velocity = 20
-        self.min_velocity = 0.001
+        #self.min_velocity = 0.001
         self.L = 0.04
-        self.max_theta = np.pi/6
+        self.max_phi = np.pi/6
         self.delta_t = 0.01 # 10 ms
         self.step = 0.1
-        self.max_acc = 5  #dobrac, [V/krok]
-        self.min_acc = -self.max_acc
-        self.max_vel = 50  #nie wiem co robie
-        self.max_ang = 30 #na razie w dec, potem podmienimy, podalem odchylenie w jedna strone
-
-    def range_velocity(self, point):
-        vel = point[2]
-        if vel + self.max_acc <= self.max_vel:
-            v_max = vel + self.max_acc
-        else:
-            v_max = self.max_vel
-        if vel + self.min_acc > 0:
-            v_min = vel + self.min_acc
-        else:
-            v_min = 1  #co z tym, przeciez nie damy zerowej?
-        return v_min, v_max
-
-    def add_few_pt(self, point, closest):
-        v_min, v_max = self.range_velocity(point)
-        points = []
-        for i in range(5):
-            v = (v_max - v_min) * i/5
-            points.append(self.new_pt(point, closest, v))
-        return points
-
-    # TODO: zaczac uzywac tej funkcji
-    def check_angle(self, point, closest):
-        return (point[2] <= closest[2] + self.max_ang) and (point[2] >= closest[2] - self.max_ang)
 
     def check_if_valid(self, a):
         if a[0] < 0 or a[0] > self.width or a[1] < 0 or a[1] > self.height:
@@ -78,11 +50,11 @@ class RRT(GridMap):
         # TODO: brac pod uwage kat i predkosc z 'closest'?
         u_s = random.random() * self.max_velocity
         # u_s = (random.random() * self.max_velocity + self.min_velocity)/(self.max_velocity + self.min_velocity)
-        u_phi = (random.random() * 2 - 1) * self.max_theta
+        u_phi = (random.random() * 2 - 1) * self.max_phi
         return u_s, u_phi
 
     def check_path(self, point_closest, point_random):
-        number_of_samples = 5
+        number_of_samples = 10
         best_point = (1e6, 1e6, 1e6)
         best_phi = 1e6
         best_s = 1e6
@@ -90,7 +62,7 @@ class RRT(GridMap):
             u_s, u_phi = self.random_u()
             valid = True
             for iteration in range(1, 101):
-                x, y, theta = self.check_point(point_closest, u_s/100 * iteration, u_phi)
+                x, y, theta = self.calc_point(point_closest, u_s/100 * iteration, u_phi)
                 if self.is_free(x, y) and self.check_if_valid((x, y)):
                     continue
                 else:
@@ -109,35 +81,39 @@ class RRT(GridMap):
             return (None, None, None), None, None
 
     # TODO: zmienic nazwy funkcji obliczajacych z check na cos innego
-    def check_point(self, point_closest, u_s, u_phi):
+    def calc_point(self, point_closest, u_s, u_phi):
         theta_t = point_closest[2] + u_s / self.L * np.tan(u_phi) * self.delta_t
         x_t = point_closest[0] + u_s * np.cos(theta_t) * self.delta_t
         y_t = point_closest[1] + u_s * np.sin(theta_t) * self.delta_t
         return x_t, y_t, theta_t
 
+    def future_check(self, point, u_s, u_phi):
+
+        for iteration in range(1, 101):
+            pass
+        return True
+
+
+
 
     def search(self):
-        """
-        RRT search algorithm for start point self.start and desired state self.end.
-        Saves the search tree in the self.parent dictionary, with key value pairs representing segments
-        (key is the child vertex, and value is its parent vertex).
-        Uses self.publish_search() and self.publish_path(path) to publish the search tree and the final path respectively.
-        """
-        # TODO: dodać predkosc samochodu
+
+        # TODO: dodac predkosc samochodu
         #  w wierzcholku powinny byc wszystkie dane samochodu (predkosc i kat kol)
         self.parent[self.start] = None
         path = [self.end]
         is_no_path = True
-
+        print("FDASLFSDAT")
         while is_no_path:
             random_point = self.random_point()
             closest = self.find_closest(random_point)
             point, u_s, u_phi = self.check_path(closest, random_point)
             # TODO: wyrzucanie punktow skierowanych w sciane
-            if point != (None, None, None):
+            if point != (None, None, None) and self.future_check(point, u_s, u_phi) == True:
                 self.parent[(point[0], point[1], point[2])] = closest
                 # print(u_phi)
             # TODO: publikowac wyszukiwanie jak trajektorie samochodu
+
             self.publish_search()
 
 
