171e45b964636bf5b38c4346c26e1d099f7b6a07
Krisis <czerwinski17@wp.pl>
2020-06-13 19:15:41 +0200

    updated rviz and car_viz

--- src/points.py
+++ src/points.py
@@ -37,7 +37,7 @@ class Point:
 if __name__ == '__main__':
     rp.init_node('points', log_level=rp.DEBUG)
     st = Point(5.8, 1.0, "start", (0.0, 1.0, 0.0))
-    en = Point(5.2, 2.7, "end", (1.0, 0.0, 0.0))
+    en = Point(6.5, 1.0, "end", (1.0, 0.0, 0.0))
     while not rp.is_shutdown():
         st.publish()
         en.publish()
